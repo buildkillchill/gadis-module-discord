7ad613260b893a19df24484f3583b74c759efa4c
halitalf <pwthomas95@zoho.com>
2018-03-05 02:40:01 -0600

    Changed dictionary from discord.Member to the respective ID

--- src/modules/antispam.py
+++ src/modules/antispam.py
@@ -18,22 +18,22 @@ class Module(common.BaseModule):
 		self.client.loop.create_task(self.silence(person, time))
 	async def punish(self, author, message):
 		await self.client.delete_message(message)
-		if self.violations[author] == None:
+		if self.violations[author.id] == None:
 			await self.client.send_message(message.channel, "There is no need to spam your keyboard {}. Your message will be removed.".format(author.mention))
-			self.violations[author] = 0
-		elif self.violations[author] < 5:
+			self.violations[author.id] = 0
+		elif self.violations[author.id] < 5:
 			await self.client.send_message(message.channel, "There is no need to spam your keyboard {}. Your message will be removed and you will be silenced for 5 minutes.".format(author.mention))
-			self.run_punishment(author, 5)
-		elif self.violations[author] < 10:
+			self.run_punishment(author.id, 5)
+		elif self.violations[author.id] < 10:
 			await self.client.send_message(message.channel, "There is no need to spam your keyboard {}. Your message will be removed and you will be silenced for 30 minutes.".format(author.mention))
-			self.run_punishment(author, 30)
-		elif self.violations[author] < 20:
+			self.run_punishment(author.id, 30)
+		elif self.violations[author.id] < 20:
 			await self.client.send_message(message.channel, "There is no need to spam your keyboard {}. Your message will be removed and you will be silenced for 1 hour.".format(author.mention))
-			self.run_punishment(author, 60)
-		elif self.violations[author] < 30:
+			self.run_punishment(author.id, 60)
+		elif self.violations[author.id] < 30:
 			await self.client.send_message(message.channel, "There is no need to spam your keyboard {}. Your message will be removed and you will be silenced for 5 hours.".format(author.mention))
-			self.run_punishment(author, 5 * 60)
-		elif self.violations[author] >= 30:
+			self.run_punishment(author.id, 5 * 60)
+		elif self.violations[author.id] >= 30:
 			msg = await self.self.client.send_message(message.channel, "{} will be kicked for spamming in 5s".format(author.mention))
 			time = 5
 			while time > 0:
@@ -43,7 +43,7 @@ class Module(common.BaseModule):
 			await self.self.client.edit_message(msg, "Kicking {}...".format(author.mention, time))
 			await self.client.kick(author)
 			await self.self.client.edit_message(msg, "Kicked {} right in the nut sack. Let 'em brew in pain for a bit. :wink:".format(author.mention, time))
-		self.violations[author] = self.violations[author] + 1
+		self.violations[author.id] = self.violations[author.id] + 1
 	async def match_del(self, pattern, message):
 		match = re.search(pattern, message.content.lower())
 		if match:
