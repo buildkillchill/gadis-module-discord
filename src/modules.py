9af9ebdcb692457260aad9c3e4776514aa811bc3
halitalf <pwthomas95@zoho.com>
2018-03-06 11:08:13 -0600

    Mod manager now can control advanced only mods

--- src/modules.py
+++ src/modules.py
@@ -3,22 +3,35 @@ import common
 
 class Module(common.BaseModule):
 	__name__ = "Mod Manager"
-	def __init__(self, enabled, client=None, modules={}):
+	def __init__(self, enabled, client=None, modules={}, advanced={}):
 		common.BaseModule.__init__(self, enabled, client)
 		self.mods = modules
+		self.advm = advanced
 		self.addcmd("enmod", self.enable, "Enable module", rank=9)
 		self.addcmd("dismod", self.disable, "Disable module", rank=9)
 	async def enable(self, args, pmsg):
 		mod = ("".join(args[1:])).lower()
+		handled = False
 		if mod in self.mods:
 			self.mods[mod].enable()
+			handled = True
+		elif mod in self.advm:
+			self.advm[mod].enable()
+			handled = True
+		if handled:
 			await self.send(pmsg.channel, "{} has been **enabled**".format(mod))
 		else:
 			await self.send(pmsg.channel, "{} does not exist".format(mod))
 	async def disable(self, args, pmsg):
 		mod = ("".join(args[1:])).lower()
+		handled = False
 		if mod in self.mods:
 			self.mods[mod].disable()
+			handled = True
+		elif mod in self.advm:
+			self.advm[mod].disable()
+			handled = True
+		if handled:
 			await self.send(pmsg.channel, "{} has been **disabled**".format(mod))
 		else:
 			await self.send(pmsg.channel, "{} does not exist".format(mod))
