a5843022fa21e358e570a11704116e032de1f67d
halitalf <pwthomas95@zoho.com>
2018-03-06 01:41:03 -0600

    Testing

--- src/modules/insultreply.py
+++ src/modules/insultreply.py
@@ -11,8 +11,8 @@ class Module(common.BaseModule):
 		msg = message.content.lower()
 		for trigger in self.db.query("SELECT * FROM `insult_triggers`"):
 			if re.search(trigger[0], msg):
-				reply = self.db.query("SELECT * FROM `insult_replies` WHERE `id`={} LIMIT 1".format(trigger[1]))
-				print(reply)
+				reply = self.db.query("SELECT * FROM `insult_replies` WHERE `id`={}".format(trigger[1]))
+				reply = reply[0]
 				if reply[2]:
 					reply_text = reply[1].format(message.author.mention)
 				else:
