cc9c917d7577ede81298edb152e6b863ae274f93
halitalf <pwthomas95@zoho.com>
2018-03-05 20:47:33 -0600

    Fix Attempt #12

--- src/modules/antispam.py
+++ src/modules/antispam.py
@@ -33,7 +33,7 @@ class SpamTables():
 	def messaged(self, contents):
 		print("{} \n\t\\\\=> {}\t\n\t{} violations\t{} identical messages\n".format(self.message(), contents.encode('utf-8', "ignore"), self.count(), self.identical()))
 		self.db.run("UPDATE `antispam` SET `timestamp`='{}' WHERE `id`={}".format(int(time.time()), self.id))
-		if contents.encode('utf-8', "ignore") == self.message().encode('utf-8', "ignore") and int(time.time()) - self.time() < 300:
+		if contents.encode('utf-8', "ignore") == self.message() and int(time.time()) - self.time() < 300:
 			self.increment("identical")
 			return True
 		else:
