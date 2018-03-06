ca0e80cdd5c5275d52e56e298c193e8da3a1e651
halitalf <pwthomas95@zoho.com>
2018-03-06 07:08:04 -0600

    Added skeleton for mod manager

---
+++ src/modules/modules.py
@@ -0,0 +1,15 @@
+import json, requests, re
+import common
+
+class Module(common.BaseModule):
+	__name__ = "Mod Manager"
+	def __init__(self, enabled, client=None):
+		common.BaseModule.__init__(self, enabled, client, True)
+		self.addcmd("enmod", self.enable, "Enable module", rank=9)
+		self.addcmd("dismod", self.disable, "Disable module", rank=9)
+	await def enable(self, args, pmsg):
+		mod = ("".join(args[1:])).lower()
+		await self.send(pmsg.channel, "This functionality is not available yet, so I cannot enable {} for you. Please use the database administration tool.".format(mod))
+	await def disable(self, args, pmsg):
+		mod = ("".join(args[1:])).lower()
+		await self.send(pmsg.channel, "This functionality is not available yet, so I cannot disable {} for you. Please use the database administration tool.".format(mod))
