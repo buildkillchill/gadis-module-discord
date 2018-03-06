7bc3b64f948264386e9256840c070f7e9cf0299c
halitalf <pwthomas95@zoho.com>
2018-03-06 01:34:19 -0600

    Migrated insults to db table

--- src/modules/insultreply.py
+++ src/modules/insultreply.py
@@ -1,18 +1,19 @@
 import json, requests, re
 import common
+import mysql
 
 class Module(common.BaseModule):
 	__name__ = "Insult Reply"
 	def __init__(self, enabled, client=None):
 		common.BaseModule.__init__(self, enabled, client)
+		self.db = mysql.default()
 	async def on_message(self, message):
 		msg = message.content.lower()
-		if msg.startswith("idc") or msg.startswith("idgaf") or msg.startswith("i dont care") or msg.startswith("i don't care") or msg.startswith("i don't give a fuck") or msg.startswith("i dont give a fuck"):
-			await self.send(message.channel, "No one gives a fuck that you don't give a fuck, so shut the fuck up and quit whining about it {}.".format(message.author.mention))
-		elif re.search(r'(@here)', msg):
-			await self.send(message.channel, "Fuck off {}".format(message.author.mention))
-		elif re.search(r'(who is)', msg):
-			await self.send(message.channel, "Who gives a fuck {}? No one, so shut up.".format(message.author.mention))
-		elif re.search(r'(fuck you)', msg):
-			await self.send(message.channel, "And why the fuck would anyone want to fuck a bot. Damn, you really are a retard {}.".format(message.author.mention))
-
+		for trigger in self.db.query("SELECT * FROM `insult_triggers`"):
+			if re.search(trigger[0], msg):
+				reply = self.db.query("SELECT * FROM `insult_replies` WHERE `id`={}".format(trigger[1]))
+				if reply[2]:
+					reply_text = reply[1].format(message.author.mention)
+				else:
+					reply_text = reply[1]
+				await self.send(message.channel, reply_text)
