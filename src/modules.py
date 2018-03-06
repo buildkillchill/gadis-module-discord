29316eed4e16e9aded5a127cb20f86ccc70958df
halitalf <pwthomas95@zoho.com>
2018-03-06 08:40:56 -0600

    Finishing up DynaFly changes

--- src/modules.py
+++ src/modules.py
@@ -3,13 +3,22 @@ import common
 
 class Module(common.BaseModule):
 	__name__ = "Mod Manager"
-	def __init__(self, enabled, client=None):
+	def __init__(self, enabled, client=None, modules={}):
 		common.BaseModule.__init__(self, enabled, client)
+		self.mods = modules
 		self.addcmd("enmod", self.enable, "Enable module", rank=9)
 		self.addcmd("dismod", self.disable, "Disable module", rank=9)
 	async def enable(self, args, pmsg):
 		mod = ("".join(args[1:])).lower()
-		await self.send(pmsg.channel, "This functionality is not available yet, so I cannot enable {} for you. Please use the database administration tool.".format(mod))
+		if mod in self.mods:
+			self.mods[mod].enable()
+			await self.send(pmsg.channel, "{} has been **enabled**".format(mod))
+		else:
+			await self.send(pmsg.channel, "{} does not exist".format(mod))
 	async def disable(self, args, pmsg):
 		mod = ("".join(args[1:])).lower()
-		await self.send(pmsg.channel, "This functionality is not available yet, so I cannot disable {} for you. Please use the database administration tool.".format(mod))
+		if mod in self.mods:
+			self.mods[mod].disable()
+			await self.send(pmsg.channel, "{} has been **disabled**".format(mod))
+		else:
+			await self.send(pmsg.channel, "{} does not exist".format(mod))
