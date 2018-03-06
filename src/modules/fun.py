de74038ec2306c2ecc53350c7f03d5defebd6235
halitalf <pwthomas95@zoho.com>
2018-03-06 00:30:22 -0600

    Added mention to joke

--- src/modules/fun.py
+++ src/modules/fun.py
@@ -20,7 +20,7 @@ class Module(common.BaseModule):
 	async def on_message(self, message):
 		msg = message.content.lower()
 		if msg.startswith("idc") or msg.startswith("idgaf") or msg.startswith("i dont care") or msg.startswith("i don't care") or msg.startswith("i don't give a fuck") or msg.startswith("i dont give a fuck"):
-			await self.send(message.channel, "No one gives a fuck that you don't give a fuck, so shut the fuck up and quit whining about it.")
+			await self.send(message.channel, "No one gives a fuck that you don't give a fuck, so shut the fuck up and quit whining about it {}.".format(message.author.mention))
 		elif re.search(r'((🐴\s?)\2{4,})', msg):
 			print(self.horses)
 			if not message.author.id in self.horses:
