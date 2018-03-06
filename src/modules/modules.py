a17ce23de05e33fef04ba764e2f8e9e2f635f520
halitalf <pwthomas95@zoho.com>
2018-03-06 07:12:55 -0600

    Fixed coro definitions

--- src/modules/modules.py
+++ src/modules/modules.py
@@ -7,9 +7,9 @@ class Module(common.BaseModule):
 		common.BaseModule.__init__(self, enabled, client, True)
 		self.addcmd("enmod", self.enable, "Enable module", rank=9)
 		self.addcmd("dismod", self.disable, "Disable module", rank=9)
-	await def enable(self, args, pmsg):
+	async def enable(self, args, pmsg):
 		mod = ("".join(args[1:])).lower()
 		await self.send(pmsg.channel, "This functionality is not available yet, so I cannot enable {} for you. Please use the database administration tool.".format(mod))
-	await def disable(self, args, pmsg):
+	async def disable(self, args, pmsg):
 		mod = ("".join(args[1:])).lower()
 		await self.send(pmsg.channel, "This functionality is not available yet, so I cannot disable {} for you. Please use the database administration tool.".format(mod))
