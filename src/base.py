4c48e7574e100bf4fa91ecc4d01300c192853d0e
halitalf <pwthomas95@zoho.com>
2018-03-07 04:13:52 -0600

    Filters non-python extensions now.

--- src/base.py
+++ src/base.py
@@ -56,8 +56,10 @@ async def on_ready():
 	files = {}
 	for filename in os.listdir('/usr/local/share/bkc-services/modules'):
 		if (filename[0] != '_' and filename[0] != '.'):
-			mname = os.path.splitext(filename)[0]
-			files[mname] = None
+			mname = os.path.splitext(filename)
+			if mname[1].startswith(".py"):
+				mname = mname[0]
+				files[mname] = None
 	sys.path.append('/usr/local/share/bkc-services/modules')
 	for key in files.keys():
 		mod = __import__(key)
