c1d75c1494b77971a8484a9a32a513bb397b2a90
halitalf <pwthomas95@zoho.com>
2018-03-11 03:04:54 -0500

    -

--- src/common.py
+++ src/common.py
@@ -225,10 +225,10 @@ class User(BaseModule):
 		for role in member.roles:
 			if "everyone" in role.name: continue
 			if int(role.id) in ranked_roles:
-				print("{} is a ranked role. Removing it from {}".format(role.id, member.id))
+				print("{} ({}) is a ranked role. Removing it from {}".format(role.id, role.name, member.id))
 				await self.client.remove_roles(member, role)
 			else:
-				print("{} is NOT a ranked role. It will remain as a group on {}.".format(role.id, member.id))
+				print("{} ({}) is NOT a ranked role. It will remain as a group on {}.".format(role.id, role.name, member.id))
 		await self.client.add_roles(member, *roles)
 		if self.rank() < rank:
 			await self.client.add_roles(member, *prev)
