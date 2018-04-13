ba1713da185543db32f0cb1edf3b78a38295dbde
halitalf <pwthomas95@zoho.com>
2018-04-13 03:36:41 -0500

    Added control and remote

---
+++ src/gadis-remote.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import socket
+import sys
+exec(open('/usr/local/share/gadis/settings.pyc').read())
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+addr = ('127.0.0.1', Settings.Remote["port"])
+sock.connect(addr)
+
+cmd = sys.argv[1]
+args = sys.argv[2:]
+
+if cmd == "send":
+	chan = sys.argv[2]
+	msg = " ".join(sys.argv[3:])
+	msg = "msg {} {}".format(chan, msg)
+	sock.send(msg.encode('utf-8'))
+elif cmd == "shutdown":
+	sock.send("shutdown".encode('utf-8'))
+elif cmd == "noadmin":
+	sock.send("noadmin {}".format(args[0]).encode('utf-8'))
+
+sock.close()
