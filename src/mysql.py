3f366119591af4dc49c5d790f580fbe243f2205f
halitalf <pwthomas95@zoho.com>
2018-04-15 22:07:34 -0500

    Added logging

--- src/mysql.py
+++ src/mysql.py
@@ -1,4 +1,5 @@
 import MySQLdb
+import logging
 from warnings import filterwarnings
 from settings import Settings
 
@@ -11,17 +12,21 @@ class MySQLWrapper():
 		self.connection.autocommit(True)
 		self.connection.ping(True)
 		self.cursor = self.connection.cursor()
+		self.logger = logging.getLogger("GADIS.MYSQL")
 		filterwarnings('ignore', category = MySQLdb.Warning)
 	def query(self, sql, replacements=None):
+		self.logger.debug("Executing '{}'".format(sql))
 		if replacements == None:
 			self.cursor.execute(sql)
 		else:
 			self.cursor.execute(sql,replacements)
 		return self.cursor.fetchall()
 	def run(self, sql, replacements=None):
+		self.logger.debug("Executing '{}'".format(sql))
 		if replacements == None:
 			self.cursor.execute(sql)
 		else:
 			self.cursor.execute(sql,replacements)
 	def disconnect(self):
+		self.logger.info("Closing DB connection")
 		self.connection.close()
