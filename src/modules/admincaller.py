121f537ef2446949d2fc45bcc02b953ceda905fe
Tinkerwolf <pwthomas95@zoho.com>
2018-05-28 11:52:13 -0500

    Added logging

--- src/modules/admincaller.py
+++ src/modules/admincaller.py
@@ -10,18 +10,24 @@ class Module(common.BaseModule):
 		client.loop.create_task(self.Check())
 	async def Check(self):
 		while True:
-			sql = "SELECT * FROM `linked` WHERE `honorary` = FALSE AND `rank` >= 7 AND `sid` IN (SELECT `id` FROM `active`);"
-			query = self.db.query(sql)
-			if len(query) == 0:
-				await self.no_admin()
+			try:
+				sql = "SELECT * FROM `linked` WHERE `honorary` = FALSE AND `rank` >= 7 AND `sid` IN (SELECT `id` FROM `active`);"
+				query = self.db.query(sql)
+				if len(query) == 0:
+					await self.no_admin()
+			except:
+				self.logger.error("An error occurred in checking for admins.")
 			asyncio.sleep(1800)
 	async def no_admin(self):
-		ch=self.getchannel("admin")
-		last=False
-		notify="<@&{}> There's no admin on shift!".format(Settings.Ranks[Settings.Admin["rank"]][0])
-		notify="There's no admin on shift!"
-		async for msg in self.client.logs_from(ch,limit=1):
-			if msg.author == self.client.user and msg.content == notify:
-				last=True
-		if not last:
-			await self.send(ch,notify)
+		try:
+			ch=self.getchannel("admin")
+			last=False
+			notify="<@&{}> There's no admin on shift!".format(Settings.Ranks[Settings.Admin["rank"]][0])
+			notify="There's no admin on shift!"
+			async for msg in self.client.logs_from(ch,limit=1):
+				if msg.author == self.client.user and msg.content == notify:
+					last=True
+			if not last:
+				await self.send(ch,notify)
+		except:
+			self.logger.error("An error occurred in notifying the admins.")
